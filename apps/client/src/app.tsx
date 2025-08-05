39bf8e275c36d16989db71dff38ea04e5f0ffa48
Jacob Dachenhaus <13964975+duckcharm@users.noreply.github.com>
2025-08-05 05:07:59 -0400

    Setup multi-page routing

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Home } from "./pages/home";
+import { NotFound } from "./pages/not-found";
+
 export function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
