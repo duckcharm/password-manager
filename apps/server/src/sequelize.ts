e1917e9030ac97019e958c1b0db21d952d32119d
Jacob Dachenhaus <13964975+duckcharm@users.noreply.github.com>
2025-08-05 06:04:55 -0400

    Add user model

--- apps/server/src/sequelize.ts
+++ apps/server/src/sequelize.ts
@@ -1,12 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
-import { basename, equalsIgnoreCase } from "./utils";
+import { equalsIgnoreCase } from "./utils";
 
 export const sequelize = new Sequelize(process.env.DB_URL!, {
   dialect: "postgres",
   models: [__dirname + "/models"],
-  modelMatch: (filename, member) => {
-    return equalsIgnoreCase(basename(filename), member);
-  },
+  modelMatch: (filename, member) => equalsIgnoreCase(filename, member),
   logging: false,
 });
 
--- apps/server/src/utils.ts
+++ apps/server/src/utils.ts
@@ -1,7 +1,3 @@
-export function basename(filename: string): string {
-  return filename.substring(0, filename.lastIndexOf("."));
-}
-
 export function equalsIgnoreCase(a: string, b: string): boolean {
   return a.toLowerCase() === b.toLowerCase();
 }
