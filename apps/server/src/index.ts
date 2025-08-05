283ba1fa825df7e4d907e1ad681c98a137765d76
Jacob Dachenhaus <13964975+duckcharm@users.noreply.github.com>
2025-08-05 05:44:51 -0400

    Bootstrap auth routes

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,8 +4,33 @@ import { createServer } from "node:http";
 const app = express();
 const server = createServer(app);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/ping", (req, res) => {
   res.send("pong");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = {
+    username,
+    password,
+  };
+
+  res.json({ user });
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = {
+    username,
+    password,
+  };
+
+  res.status(201).json({ user });
 });
 
 const port = process.env.PORT || 3000;
